a884aa2c254bba0650639b5a9ee427e7557efec0
igor_pont <pontigor11@gmail.com>
2022-06-19 21:15:01 +0300

    Updated the connection of the modules

--- data_generation.py
+++ data_generation.py
@@ -1,26 +1,33 @@
-import csv 
-file = open('base_phone.csv', 'w') 
-newrecord ="ID;Name;Surname;PhoneNumber;e_mail\n" 
-file.writelines(newrecord) 
- 
-ls_name = ['Svetlana', 'Olga', 'Anton', 'Anna', 'Inna', 'Viktor', 'Vasilisa', 'Alex', 'Miron','Igor', 'Anna'] 
-ls_surname = ['Kovalenko', 'Sidorenko', 'Mironenko', 'Galich', 'Shapiro', 'Duma', 'Duma', 'Shagal', 'Moroz'] 
+import random
+import csv
+file = open('base_phone.csv', 'w')
+newrecord = "ID;Name;Surname;PhoneNumber;e_mail\n"
+file.writelines(newrecord)
+
+ls_name = ['Svetlana', 'Olga', 'Anton', 'Anna', 'Inna',
+           'Viktor', 'Vasilisa', 'Alex', 'Miron', 'Igor', 'Anna']
+ls_surname = ['Kovalenko', 'Sidorenko', 'Mironenko',
+              'Galich', 'Shapiro', 'Duma', 'Duma', 'Shagal', 'Moroz']
 ls_e_mail = ['@gmail.com', '@yandex.ru', '@mail.ru']
-import random 
- 
-def list_of_numbers(): 
-    s = '+' 
-    randomListPhone = random.randint(79000000000, 80000000000)    
-    s = s + str(randomListPhone) 
-    return s 
-# рандомно соеденить имена фамилии и номера телефонов (отдельный метод) def + присвоение id  
- 
-def string_creation(): 
-    s = "" 
-    s = s + random.choice(ls_name) + ';' + random.choice(ls_surname) + ';' +  list_of_numbers() + ';' + random.choice(ls_surname) + random.choice(ls_e_mail)
-    return s 
- 
-for i in range(100): 
-    a = f'{str(i + 1)};{string_creation()}\n' 
-    file.write(a) 
-file.close()
+
+
+def list_of_numbers():
+    s = '+'
+    randomListPhone = random.randint(79000000000, 80000000000)
+    s = s + str(randomListPhone)
+    return s
+# рандомно соеденить имена фамилии и номера телефонов (отдельный метод) def + присвоение id
+
+
+def string_creation():
+    s = ""
+    s = s + random.choice(ls_name) + ';' + random.choice(ls_surname) + ';' + \
+        list_of_numbers() + ';' + random.choice(ls_surname) + random.choice(ls_e_mail)
+    return s
+
+
+def start():
+    for i in range(100):
+        a = f'{str(i + 1)};{string_creation()}\n'
+        file.write(a)
+    file.close()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# import data_generation as dg
 import user_interface as ui
 import logger as lg
 import crud
 
+
+# чисто технически можо получать имя файла бд из переменных среды или конфигурационного файла
+# dg.start()
 lg.logging.info('Start')
-crud.init_data_base('base_phone.csv') # чисто технически можо получать имя файла бд из переменных среды или конфигурационного файла
+crud.init_data_base('base_phone.csv')
 ui.ls_menu()
